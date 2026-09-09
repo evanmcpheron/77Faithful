991eea2469971ed2336cfd198bd804dd9824c39f
Evan McPheron <evan.mcpheron@icloud.com>
2026-09-09 10:44:23 -0400

    updates to routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,49 @@
-import { SeventySevenScreenPlaceholder } from '@77/components/core';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { YStack } from 'tamagui';
 
-const LoginScreen = () => <SeventySevenScreenPlaceholder title="Log In" />;
+import {
+  SeventySevenButton,
+  SeventySevenText,
+  SeventySevenTextAlignment,
+  SeventySevenTextSize,
+} from '@77/components/core';
 
-export default LoginScreen;
+const WelcomeScreen = () => {
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <YStack
+      flex={1}
+      bg="$background"
+      pt={safeAreaInsets.top}
+      pb={safeAreaInsets.bottom}
+      pl={safeAreaInsets.left}
+      pr={safeAreaInsets.right}
+    >
+      <YStack flex={1} items="center" justify="center" px="$4" py="$6">
+        <YStack width="100%" maxW={400} gap="$6">
+          <YStack gap="$3">
+            <SeventySevenText
+              size={SeventySevenTextSize.Heading}
+              alignment={SeventySevenTextAlignment.Center}
+              role="heading"
+              aria-level={1}
+              allowFontScaling
+            >
+              Welcome to 77Faithful
+            </SeventySevenText>
+            <SeventySevenText alignment={SeventySevenTextAlignment.Center} allowFontScaling>
+              A faithful rhythm, one day at a time.
+            </SeventySevenText>
+          </YStack>
+          <YStack gap="$3">
+            <SeventySevenButton href="/login">Sign in</SeventySevenButton>
+            <SeventySevenButton href="/register">Register</SeventySevenButton>
+          </YStack>
+        </YStack>
+      </YStack>
+    </YStack>
+  );
+};
+
+export default WelcomeScreen;
---
+++ src/app/login.tsx
@@ -0,0 +1,5 @@
+import { SeventySevenScreenPlaceholder } from '@77/components/core';
+
+const LoginScreen = () => <SeventySevenScreenPlaceholder title="Log In" />;
+
+export default LoginScreen;
